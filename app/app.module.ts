084037f48a7600a2fa30aeace81030fcc9df48ff
graphicsxp <graphicsx@gmail.com>
2016-09-21 16:54:39 +0200

    added styles + index.html file + bootstrap code

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component';
+
 @NgModule({
     imports: [BrowserModule],
     exports: [],
-    declarations: [],
+    declarations: [AppComponent],
     providers: [],
+    bootstrap: [AppComponent]
 })
-export class NameModule { }
+export class AppModule { }
